OWCSX/blog_os

Create a Writer struct with a `write_byte` function

src/vga_buffer.rs
@@ -1,4 +1,7 @@
+use core::ptr::Unique;
+
 #[allow(dead_code)]
+#[derive(Debug, Clone, Copy)]
 #[repr(u8)]
 pub enum Color {
     Black      = 0,
@@ -19,6 +22,7 @@ pub enum Color {
     White      = 15,
 }
 
+#[derive(Debug, Clone, Copy)]
 struct ColorCode(u8);
 
 impl ColorCode {
@@ -27,6 +31,7 @@ impl ColorCode {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
 #[repr(C)]
 struct ScreenChar {
     ascii_character: u8,
@@ -38,4 +43,39 @@ const BUFFER_WIDTH: usize = 80;
 
 struct Buffer {
     chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: Unique<Buffer>,
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer().chars[row][col] = ScreenChar {
+                    ascii_character: byte,
+                    color_code: color_code,
+                };
+                self.column_position += 1;
+            }
+        }
+    }
+
+    fn buffer(&mut self) -> &mut Buffer {
+        unsafe{ self.buffer.get_mut() }
+    }
+
+    fn new_line(&mut self) {/* TODO */}
 }

ivanium/RustOS

src/vga_buffer.rs
@@ -1,4 +1,7 @@
+use core::ptr::Unique;
+
 #[allow(dead_code)]
+#[derive(Debug, Clone, Copy)]
 #[repr(u8)]
 pub enum Color {
     Black      = 0,
@@ -19,6 +22,7 @@ pub enum Color {
     White      = 15,
 }
 
+#[derive(Debug, Clone, Copy)]
 struct ColorCode(u8);
 
 impl ColorCode {
@@ -27,6 +31,7 @@ impl ColorCode {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
 #[repr(C)]
 struct ScreenChar {
     ascii_character: u8,
@@ -38,4 +43,39 @@ const BUFFER_WIDTH: usize = 80;
 
 struct Buffer {
     chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: Unique<Buffer>,
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer().chars[row][col] = ScreenChar {
+                    ascii_character: byte,
+                    color_code: color_code,
+                };
+                self.column_position += 1;
+            }
+        }
+    }
+
+    fn buffer(&mut self) -> &mut Buffer {
+        unsafe{ self.buffer.as_mut() }
+    }
+
+    fn new_line(&mut self) {/* TODO */}
 }